a7621c57bb1597d383165e7b501cf1ddcb4b7bf4
unknown <vamseekrishna557@gmail.com>
2022-09-18 17:34:01 +0530

    This is my first script

---
+++ common.sh/common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+common_fun() {
+    echo "_________ Function Start ________"
+    echo "My funciton name is common_fun"
+    echo "I am called from common.sh"
+    echo "_________ Function End ________"
+}
