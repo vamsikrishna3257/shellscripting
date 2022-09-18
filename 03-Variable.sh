cb915f7d1dae06f73654c0de0d287fa0745109a5
unknown <vamseekrishna557@gmail.com>
2022-09-18 11:39:50 +0530

    This is my first script

--- 03-Variable.sh
+++ 03-Variable.sh
@@ -1,15 +1,30 @@
-#!/bin/bash
+#!/bin/bash 
 
-#A variable holds the value dynamically
-a=10
-#10 is a string here
+### A Variable is something whichs holds the values dynamically.
+
+a=10 
+# 10 s a string here 
 b=abc
+d=300
 
-#abc is also a string
 
-#when to use double quotes?? When the input is having any special charecters we need to use special charecters
+# abc is also a string 
 
-echo $a   #$a is used to print the value of a
+# When to use Double Quotes ??? When your input us having any special characters, then you need to enclose them in QUOTES.
 
-echo $b 
-echo "the value of c is $c"
+echo $a  # $a is printing the value of a 
+echo $b
+echo ${b}
+echo "The value of c is $c"
+
+echo Value of d is $d    ### Accessing a varianble from shell
+
+# rm -rf /data/${DATA_DIR} ---> /data/
+
+# $a or  ${a}
+
+# Varaibles that are declared inside the scripts are local variables 
+# Variables that are declared as shell using export are environment variables. When the values are not consistent and would based on the running, then go with env variables.
+
+# Always, local varaibles > Env Variables 
+# Local variables will have higher precedence than ENV Variables
