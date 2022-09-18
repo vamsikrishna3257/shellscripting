9059d27c98f16188274afa7a0acbb2630f0d1ffc
unknown <vamseekrishna557@gmail.com>
2022-09-18 11:15:50 +0530

    This is my first script

---
+++ 03-Variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#A variable holds the value dynamically
+a=10
+#10 is a string here
+b=abc
+
+#abc is also a string
+
+#when to use double quotes?? When the input is having any special charecters we need to use special charecters
+
+echo $a   #$a is used to print the value of a
+
+echo $b 
