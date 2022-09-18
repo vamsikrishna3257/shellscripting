aeea5869af4f97269d9c1f1a13bab99d1e5b9e43
unknown <vamseekrishna557@gmail.com>
2022-09-18 16:48:52 +0530

    This is my first script

--- 05-specialVariables.sh
+++ 05-specialVariables.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
+#!/bin/bash 
 
-#$0: This will print the script name that you are running
-#$?: This will show you the status code of last execution
 
-echo "Script we are running is $0"
+### Special Variables 
+# $0  : This will print the script name that you are running 
+# $?  : This will show your the status code of the last execution
+# $0 to $n , $* , $@ , $# , $$
+a=100
+b=300
+echo "Declared values are $a $b"
+echo "Script we are executing is $0"
+
+# Through command line you can pass up to 9 variables 
+
+echo "The value of X is $1"
+echo "Name of the Trainer is $2 and this is $3 batch"
+
+# sh script.sh 10  20  30 
+#              $1  $2  $3
+
+
+echo "CLI Variables used are $*"  # This will print all the cli variables that we are using in the script.
+echo "Number of vairables are $# "
