57703f37124fc52f15accb663f48aacef079e99b
unknown <vamseekrishna557@gmail.com>
2022-09-18 09:34:34 +0530

    This is my first script

--- 01-sample.sh
+++ 01-sample.sh
@@ -2,3 +2,11 @@
 
 #first line should always be SHEBANG Notataion and # can be used as comment
 echo "Hello World"
+
+#printing two lines
+echo "hai"
+echo "hello"
+
+## \n : new line ; \t: new tab
+
+echo -e "hai\nhello"
