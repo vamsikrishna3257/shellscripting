51f38ae03bfa7c55496bdf42c2a232d98b2cf758
unknown <vamseekrishna557@gmail.com>
2022-09-19 16:27:36 +0530

    This is my first script

---
+++ common.sh/Roboshop/Components/frotndend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Hello, I am frontend"
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
---
+++ common.sh/Roboshop/Components/main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sh components/$1.sh
+
